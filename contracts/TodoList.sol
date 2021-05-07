428fd923bd3895d0e238cfed2fdea2c45dfd7467
James Jefferson <jrj72@Jamess-Mac-mini.local>
2021-05-06 22:38:28 -0500

    task function built

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0;
-}
+    uint256 public taskCount = 0;
 
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Deez Nutz");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+}
