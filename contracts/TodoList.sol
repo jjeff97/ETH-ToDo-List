85b999aca18309f34ce358bd541768f5f3acfbe1
James Jefferson <jrj72@Jamess-Mac-mini.local>
2021-05-12 20:33:15 -0500

    Connecting and adding function to client

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint256 public taskCount = 0;
+    uint public taskCount = 0;
 
     struct Task {
-        uint256 id;
+        uint id;
         string content;
         bool completed;
     }
